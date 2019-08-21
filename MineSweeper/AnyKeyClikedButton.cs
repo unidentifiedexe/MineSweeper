9812f5874b18d6c6560c7b166d1963a21c593b68
Uniden <kakuninhuka@gmail.com>
2019-08-22 01:23:19 +0900

    とりあえず上げる

---
+++ MineSweeper/AnyKeyClikedButton.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MineSweeper
+{
+    ///// <summary>
+    ///// このカスタム コントロールを XAML ファイルで使用するには、手順 1a または 1b の後、手順 2 に従います。
+    /////
+    ///// 手順 1a) 現在のプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    ///// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    ///// 追加します:
+    /////
+    /////     xmlns:MyNamespace="clr-namespace:MineSweeper"
+    /////
+    /////
+    ///// 手順 1b) 異なるプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    ///// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    ///// 追加します:
+    /////
+    /////     xmlns:MyNamespace="clr-namespace:MineSweeper;assembly=MineSweeper"
+    /////
+    ///// また、XAML ファイルのあるプロジェクトからこのプロジェクトへのプロジェクト参照を追加し、
+    ///// リビルドして、コンパイル エラーを防ぐ必要があります:
+    /////
+    /////     ソリューション エクスプローラーで対象のプロジェクトを右クリックし、
+    /////     [参照の追加] の [プロジェクト] を選択してから、このプロジェクトを参照し、選択します。
+    /////
+    /////
+    ///// 手順 2)
+    ///// コントロールを XAML ファイルで使用します。
+    /////
+    /////     <MyNamespace:AnyKeyClikedButton/>
+    /////
+    ///// </summary>
+    
+    /// <summary>
+    /// マウスの右クリック、ホイールクリックに対応したボタンです
+    /// </summary>
+    public class AnyKeyClikedButton : Button
+    {
+        static AnyKeyClikedButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnyKeyClikedButton), new FrameworkPropertyMetadata(typeof(AnyKeyClikedButton)));
+        }
+
+
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            //base.OnMouseDown(e);
+
+            IsPressed = true;
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            if (e.RightButton == MouseButtonState.Released &&
+            e.LeftButton == MouseButtonState.Released)
+            {
+
+                IsPressed = false;
+            }
+        }
+    }
+}
---
+++ MineSweeper/CellState.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+
+    struct CellState
+    {
+        public CellType Type { get; }
+
+        const uint _maxNumber = 8;
+        public int Number { get; }
+
+        /// <summary> セルの状態が初期値かどうかを判断します </summary>
+        public bool IsDefault => Type == CellType.Error;
+
+        public CellState(CellType type)
+        {
+
+            if (type == CellType.Number)
+                Type = CellType.Error;
+            else
+                Type = type;
+
+            Number = 0;
+        }
+
+        public CellState(int number)
+        {
+            Number = number;
+            if (number == 0)
+                Type = CellType.Empty;
+            else if ((uint)Number < _maxNumber)
+                Type = CellType.Number;
+            else
+                Type = CellType.Error;
+        }
+
+        private CellState(CellType type, int number)
+        {
+            Type = type;
+            Number = number;
+        }
+
+        static public implicit operator CellState(int val)
+        {
+            return new CellState(val);
+        }
+
+        static public implicit operator CellState(CellType type)
+        {
+            return new CellState(type);
+        }
+
+    }
+
+    enum CellType
+    {
+        /// <summary> 不正値(未定義)  </summary>
+        Error = 0,
+        /// <summary> 存在しない </summary>
+        None ,
+        /// <summary> 爆弾 </summary>
+        Bomb,
+        /// <summary> 旗 </summary>
+        Flag,
+        /// <summary> カラ(=0) </summary>
+        Empty,
+        /// <summary> 数字 </summary>
+        Number,
+        /// <summary> ミスフラッグ </summary>
+        WrongFlag,
+        /// <summary> 未オープン </summary>
+        UnOpend,
+    }
+
+    static class CellTypeHelper
+    {
+
+        static private void ThrowHelper(string name)
+        {
+            throw new ArgumentException(name);
+        }
+
+       public static bool? IsOpend(this CellType type)
+        {
+            switch (type)
+            {
+                case CellType.None:
+                    return null;
+                case CellType.Bomb:
+                case CellType.Flag:
+                case CellType.Empty:
+                case CellType.Number:
+                case CellType.WrongFlag:
+                    return true;
+                case CellType.UnOpend:
+                    return false;
+                default:
+                    ThrowHelper(nameof(type));
+                    return null;
+            }
+        }
+    }
+
+
+}
